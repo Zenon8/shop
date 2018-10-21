4a23da4f330f5c23137d6d0b9addad7ae928562c
Andrii_Bezruchko <Andrii_Bezruchko@epam.com>
2018-10-22 02:53:40 +0300

    horizontal-tab-menu

---
+++ src/app/core/components/horizontal-tab-menu/horizontal-tab-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-horizontal-tab-menu',
+  templateUrl: './horizontal-tab-menu.component.html',
+  styleUrls: ['./horizontal-tab-menu.component.css']
+})
+export class HorizontalTabMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
